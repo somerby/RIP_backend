a22696626ab3ff0027afcaff458efd0785750768
somerby <romchikkud321@gmail.com>
2024-10-24 23:46:56 +0300

    primary discharge

---
+++ radioactive_elements/radioactive_elements_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RadioactiveElementsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'radioactive_elements_app'
